3e3168488f15942e0c4ccc6044085079759adcac
HajarIH <languages0701@gmail.com>
2024-01-02 06:26:08 -0800

    square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Module to print_square method"""
+
+def print_square(size):
+    """Method for printing square
+
+    Args:
+       size: the size of square
+
+    Raises:
+      TypeError: if size is not an integer
+      ValueError: if size is less than 0
+      TypeError: if size is a float and is less than 0
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    print((("#" * size + "\n") * size), end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
