2cd06177f8ea1ca25ac6e4dc31c5d2e4dc058ec3
HajarIH <languages0701@gmail.com>
2024-01-14 11:31:31 -0800

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for class square"""
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """a square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constractor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """prints [Square] (<id>) <x>/<y> - <size>"""
+        return "[{}] ({}) {}/{} - {}".format(type(self).__name__, self.id, self.x, self.y, self.width)
