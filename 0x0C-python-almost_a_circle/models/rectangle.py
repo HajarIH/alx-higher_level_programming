119f66c9ea0e7aa8c0baaa80cc2709af04fd6080
HajarIH <languages0701@gmail.com>
2024-01-14 09:01:09 -0800

    update 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,23 @@ class Rectangle(Base):
     def __str__(self):
         """ returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
         return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__, self.id, self.x, self.y, self.width, self.height) 
+
+    def sub_update(self, id=None, width=None, height=None, x=None, y=None):
+        """sub method to update attributes"""
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """a method to update attributes"""
+        if args:
+            self.sub_update(*args)
+        elif kwargs:
+            self.sub_update(**kwargs)
