ec0ca9a87b6700b661a8f87e4b91f06fd66af318
HajarIH <languages0701@gmail.com>
2024-01-14 11:43:29 -0800

    square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,3 +23,21 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def sub_update(self, id=None, size=None, x=None, y=None):
+        """sub method to update attributes"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """a method to update attributes"""
+        if args:
+            self.sub_update(*args)
+        elif kwargs:
+            self.sub_update(**kwargs)
