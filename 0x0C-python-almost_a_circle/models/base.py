ed4bbadbbe6447d7e0e1a37ac0439b5813ce1045
HajarIH <languages0701@gmail.com>
2024-01-14 12:05:42 -0800

    dictionnary to json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module for class Base"""
+from json import dumps, loads
 
 class Base:
     """The class Base"""
@@ -11,3 +12,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None:
+            return []
+        else:
+            return dumps(list_dictionaries)
