fcb225463db1122f0062fa341b65300554c61f35
HajarIH <languages0701@gmail.com>
2024-01-20 13:12:19 -0800

    inherite

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""inherits from"""
+
+
+def inherits_from(obj, a_class):
+    """ a function that returns True if the object is an
+    instance of a class that inherited 
+    from the specified class ; otherwise False"""
+    if issubclass(obj, a_class) and type(obj) != a_class:
+        return True
+    return False
