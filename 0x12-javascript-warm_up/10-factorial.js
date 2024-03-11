5ad6c4c7238202c708d66a0bb58aa6ba59786a0f
HajarIH <languages0701@gmail.com>
2024-03-11 21:11:46 +0000

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function factorial (a) {
+  if (isNaN(a) || a === 0) {
+    return (1);
+  }
+  if (a < 0) {
+    return (-1);
+  }
+  return (a * factorial (a - 1));
+}
+
+console.log(factorial(Number(process.argv[2])));
