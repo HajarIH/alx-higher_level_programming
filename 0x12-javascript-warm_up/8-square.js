16702e59224f9d214daf256802506cdb3895816b
HajarIH <languages0701@gmail.com>
2024-03-11 20:53:51 +0000

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const i = Number(process.argv[2]);
+  let j = 0;
+  while (j < i) {
+    console.log('X'.repeat(i));
+    j++;
+  }
+}
