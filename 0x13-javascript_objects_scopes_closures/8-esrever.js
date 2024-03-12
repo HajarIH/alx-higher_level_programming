e59afb3ebaf3fe5e70fba7c717d9c141bbd03ca8
HajarIH <languages0701@gmail.com>
2024-03-12 16:53:16 +0000

    reverse

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let r;
+  const l = (list.length - 1) * 0.5
+  for (let i = 0; i < l; i++) {
+    r = list[i];
+    list[i] = list[list.length - i - 1];
+    list[list.length - i - 1] = r;
+  }
+  return list;
+};
