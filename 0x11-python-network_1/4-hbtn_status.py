3e8b773dc008c3ad34e763c6144c1f1a54c0f898
HajarIH <languages0701@gmail.com>
2024-03-28 22:26:30 +0000

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,8 +2,9 @@
 """a script that fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
-url = 'https://alx-intranet.hbtn.io/status'
-response = requests.get(url)
-print("Body response:")
-print("\t- type:", type(response.text))
-print("\t- content:", response.text)
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
