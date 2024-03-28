995519963eb3c5bb465709759754ae233a59247b
HajarIH <languages0701@gmail.com>
2024-03-28 22:10:13 +0000

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,10 +5,10 @@ import sys
 import urllib.request
 import urllib.parse
 
-url = sys.argv[1]
-email = sys.argv[2]
-
-data = urllib.parse.urlencode({'email': email})
-data = data.encode('utf-8')
-with urllib.request.urlopen(url, data=data) as response:
-    print("Your email is: ", email)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
+    with urllib.request.urlopen(url, data=data) as response:
+        print("Your email is: ", email)
