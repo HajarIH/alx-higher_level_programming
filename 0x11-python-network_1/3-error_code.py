a93a4294e400a56ec2b3578f3a6e1494a4c20f56
HajarIH <languages0701@gmail.com>
2024-03-28 22:11:52 +0000

    error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,10 +5,11 @@ import sys
 import urllib.request
 import urllib.error
 
-url = sys.argv[1]
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
-        print(body)
-except urllib.error.HTTPError as err:
-    print(f"Error code: {err.code}")
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as err:
+        print(f"Error code: {err.code}")
